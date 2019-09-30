1d0e2bee41e18c8a2dbc7014c4230caeed202bd2
AndresADAits <45296894+AndresADAits@users.noreply.github.com>
2019-09-30 20:45:31 +0200

    primer commit del tema2

---
+++ ejemplo1.js
@@ -0,0 +1,33 @@
+console.log("Mi primera funcion");
+a=11;
+/* si hago esto
+ const a=1;
+ const a=2;
+ o esto
+ let a=1;
+ let a=2;
+ dara fallo
+ aunque le de el mismo valor, porque estas declarando dos veces la misma
+ variable.
+ 
+ Con let puede hacer esto let a=1;
+ a+=2
+
+ a seria igual a 3
+
+ Con const no puedes porque debe ser una constante.
+
+ Si lo hago con var
+ o declarando directamente da el ultimo valor
+ a=1; var a=1;
+ a=2; var a=2;
+ */
+function prueba(){
+    console.log("Ejecutando funcion prueba");
+    return a;
+    
+    /*los ambitos suelen estar definidos por estar entre llaves {} excepto el principal
+
+    
+*/
+}
