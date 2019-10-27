9e89a273ad236dac76a869c9197d10beca3f88a0
JAndres <joanferfra@hotmail.com>
2019-10-27 16:05:12 +0100

    palLobby

--- ejercicios/tema3/expresionesRegulares.js
+++ ejercicios/tema3/expresionesRegulares.js
@@ -19,26 +19,51 @@ function esValidaURL(url) {
 //Tiene que tener al menos una letra Mayúscula
 //Ayuda: Pueden usarse varias expresiones regulares para validar el password
 function esValidaPassword(password) {
-   
-return /^(?=.+[a-z])(?=.+[A-Z])(?=.+[0-9])(?=.+[\!\?\-\$\_])[A-Za-z-0-9\!\?\-\$\_]{9,}/.test(password);
+    let recorreCaracter = password.split("");
+    let cuentaNumeros = 0;
+    let cuentaLetras = 0;
+    for (let i = 0; i < recorreCaracter.length; i++) {
+        console.log(recorreCaracter[i] + " ");
+        if (/[0-9]/.test(recorreCaracter[i])) {
+            cuentaNumeros++;
+            console.log(cuentaNumeros);
+        } else {
+            //no pasa nada
+        }
+        //le podrias meter ñ? /[a-zñ]/
+        if (/[a-z]/.test(recorreCaracter[i])) {
+            cuentaLetras++;
+            console.log(cuentaLetras);
+        } else {
+            //no pasa nada
+        }
+    }
+
+
+    if ((cuentaNumeros === 4) && (cuentaLetras === 3)) {
+        return /^(?=.+[a-z])(?=.+[A-Z])(?=.+[0-9])(?=.+[\!\?\-\$\_])[A-Za-z-0-9\!\?\-\$\_]{9,}/.test(password);
+    } else {
+        return false;
+    }
+
 }
 
 //Función que elimina todos los caracterés que no sean letras y
 //números de una texto
 function eliminaCaracteresRaros(texto) {
 
-/**
- *
- * let terminator=/([^a-zA-Z0-9])/g;
-let loOtro=terminator.test(texto);
+    /**
+     *
+     * let terminator=/([^a-zA-Z0-9])/g;
+    let loOtro=terminator.test(texto);
+    
+    if (loOtro){
+        return texto.replace(/[^a-zA-Z0-9]/g, "");
+    }
+     */
 
-if (loOtro){
-    return texto.replace(/[^a-zA-Z0-9]/g, "");
-}
- */
-
-/**
- * en una linea
- */
-return /([^a-zA-Z0-9])/.test(texto) ?  texto.replace(/[^a-zA-Z0-9]/g, "") : texto
+    /**
+     * en una linea
+     */
+    return /([^a-zA-Z0-9])/.test(texto) ? texto.replace(/[^a-zA-Z0-9]/g, "") : texto
 }
