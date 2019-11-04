e2db3de81217b6a5cd445e1d8ca63aa37cf89a58
AndresADAits <45296894+AndresADAits@users.noreply.github.com>
2019-11-04 19:21:39 +0100

    nose

---
+++ ejercicios/tema3/clase30_10.js
@@ -0,0 +1,26 @@
+/**
+ * CREA UNA FUNCION QUE RECIBA POR PARAMETROS UNA LISTA DE NUMEROS 
+ * Y DEVUELVE UNA FUNCION QE DANDO UN PARAMETRO Y , SUME TODOS LOS NUEMROS
+ *  DE LA LSITA QUE SEAN DIVISIBLES POR "Y"
+ */
+
+function recibeNumeros(numeros) {
+
+
+    return function (y) {
+        let suma=0;
+        for (let i = 0; i < numeros.length; i++) {
+            if (numeros[i] % y === 0) {
+                suma+= numeros[i];
+            }
+
+        }
+return suma;
+    }
+
+}
+
+let prueba1= recibeNumeros([1,2,3,4,5,6,7,8,9,]);
+
+console.log(recibeNumeros(2));
+console.log(recibeNumeros(3));
