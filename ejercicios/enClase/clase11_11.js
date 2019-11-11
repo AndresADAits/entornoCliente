9646372aeed5b1028ef5f577214cdf5bf6d5f412
AndresADAits <45296894+AndresADAits@users.noreply.github.com>
2019-11-11 19:26:38 +0100

    primero del dia

--- ejercicios/enClase/clase11_11.js
+++ ejercicios/enClase/clase11_11.js
@@ -4,6 +4,24 @@
  * lOS DIVS IMPARES DEBEN TENER COLOR VERDE Y LOS PARE COLOR ROJO.
  * cADA DIV DEBE MOSTARAR SU NUMERO CORRESPONDIENTE CON UN H2 POR EJEMPLO: DIV 1 */
 
- function creaDivs(numero){
+function creaDivs(numero) {
+    for (let i = 1; i <= numero; i++) {//esto lo hago para luego no poner +1 en linea 11 y que me cuadre lo de par impar por color
 
- }
+        let div = document.createElement("div");
+        let tamano = 100 * i;
+        let h1=document.createElement("h1");
+        h1.innerHTML=`DIV${i}`;
+        div.style.width = `${tamano}px`;
+        div.style.height = `${tamano}px`;
+
+        if(i%2===0){
+div.style.backgroundColor='red';
+
+        }else{
+            div.style.backgroundColor='green';
+        }
+div.style.border='1px solid black';
+document.body.appendChild(div);
+div.appendChild(h1);
+    }
+}
